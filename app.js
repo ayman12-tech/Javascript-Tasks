bc0a19da31a71360a8b1c4951687e2e5a23f89a9
ayman12-tech <syedaayman8@gmail.com>
2020-06-02 22:29:33 +0500

    Commit Complete

---
+++ app.js
@@ -0,0 +1,16 @@
+//CHAPTER 1
+
+//Task1. Write a script to greet your website visitor using JS alert box.
+alert("Hello! Welcome to our store");
+//Task2. Write a script to display following message on your web page: 
+alert("Error!Please enter a valid password.");
+//Task3. Write a script to display following message on your web page: (Hint : Use line break) 
+alert("Welcome JS Land ...\nHappy Coding!");
+//Task4.  Write a script to display following messages in sequence: 
+alert("Welcome to Js Land....");
+alert("Happy Coding!");
+//Task 5. Generate the following message through browser’s developer console: 
+alert("Hello! I can run alert through web browser's console.");
+//Task 6. Make use of alerts in your new/existing HTML & CSS project. 
+alert("Heyaa!");
+
